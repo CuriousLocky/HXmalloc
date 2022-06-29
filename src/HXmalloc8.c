289ea17179f3244f9b12020f8743554c05457224
Hexiang Geng <hexiang@nclab.kaist.ac.kr>
2022-06-29 17:23:40 +0900

    Add 8-byte block find victim logic

--- src/HXmalloc8.c
+++ src/HXmalloc8.c
@@ -3,14 +3,15 @@
 #include "HXmalloc8.h"
 #include "NonblockingStack.h"
 
-thread_local uint64_t *localChunk8;
+thread_local uint64_t *localChunk8 = NULL;
 volatile NonBlockingStackBlock readyChunk8Stack = {.block_16b=0};
+volatile NonBlockingStackBlock cleanChunk8Stack = {.block_16b=0};
 
-void Block8SetNext(Block8 *prevBlock, Block8 *nextBlock){
+static void Block8SetNext(Block8 *prevBlock, Block8 *nextBlock){
     *((Block8 **)prevBlock) = nextBlock;
 }
 
-Block8 *Block8GetNext(Block8 *prevBlock){
+static Block8 *Block8GetNext(Block8 *prevBlock){
     return (Block8*)(*((Block8**)prevBlock));
 }
 
@@ -35,7 +36,8 @@ void HXfree8(Block8 *block){
     }
 }
 
-Block8 *findLocalVictim(){
+static Block8 *findLocalVictim(){
+    // assuming local chunk is not NULL
     if(localChunk8[0] != 0){
         uint64_t slotMask = _blsi_u64(localChunk8[0]);
         int index = _tzcnt_u64(slotMask);
@@ -48,9 +50,41 @@ Block8 *findLocalVictim(){
         __atomic_fetch_and(localChunk8 + 1, (~slotMask), __ATOMIC_RELAXED);
         return localChunk8 + (2 * index);
     }
-    // local chunk is full, set as cleaning
-    __atomic_fetch_or(localChunk8, BLOCK8_CLEANING_FLAG, __ATOMIC_RELAXED);
 
     return NULL;
 }
 
+static void getNewLocalChunk(){
+    // check ready stack
+    Block8 *newLocakChunk = pop_nonblocking_stack(readyChunk8Stack, Block8GetNext);
+    if(newLocakChunk != NULL){
+        localChunk8 = newLocakChunk;
+        return;
+    }
+    // check clean stack
+    newLocakChunk = pop_nonblocking_stack(cleanChunk8Stack, Block8GetNext);
+    if(newLocakChunk != NULL){
+        localChunk8 = newLocakChunk;
+        return;
+    }
+    // get new chunk from memory pool
+    // TODO: implement new chunk creation
+}
+
+Block8 *FindVictim8(){
+    Block8 *result = NULL;
+    // check local chunk
+    result = findLocalVictim();
+    if(result != NULL){
+        return result;
+    }
+    
+    // local chunk is full, set as cleaning
+    __atomic_fetch_or(localChunk8, BLOCK8_CLEANING_FLAG, __ATOMIC_RELAXED);
+    localChunk8 = NULL;
+
+    // get a new local chunk and try again
+    getNewLocalChunk();
+    // since only this thread can allocate from the chunk, it will always success
+    return findLocalVictim(); 
+}
