a4a21bb0a612b8358d0cb301970f3dd600b56ed7
Hexiang Geng <hexiang@nclab.kaist.ac.kr>
2022-07-04 11:39:49 +0900

    Add generation rule for small blocks

--- codegen.py
+++ codegen.py
@@ -1,9 +1,7 @@
 from jinja2 import Template
 import os
 
-# generate small block rules
-blockSize = 16
-
+# check generate dir existence
 genSrcPath = "generated/src/"
 genIncludePath = "generated/include/"
 
@@ -11,14 +9,23 @@ if(not os.path.exists(genSrcPath)):
     os.makedirs(genSrcPath)
 if(not os.path.exists(genIncludePath)):
     os.makedirs(genIncludePath)
+    
+# generate small block rules
+smallBlockHeaderFileName = genIncludePath + "smallBlock.h"
+smallBlockHeader = ""
+smallBlockSizes = [16, 32, 64]
 
-with open("src/smallBlockTemplate.c") as templateFile:
-    template = Template(templateFile.read())
-    srcFileName = genSrcPath + "smallBlock{blockSize}.c".format(blockSize = blockSize)
-    with open(srcFileName, 'w') as outputFile:
-        outputFile.write(template.render(blockSize = blockSize))
-with open("include/smallBlockTemplate.h") as templateFile:
-    template = Template(templateFile.read())
-    includeFileName = genIncludePath + "smallBlock{blockSize}.h".format(blockSize = blockSize)
-    with open(includeFileName, "w") as outputFile:
-        outputFile.write(template.render(blockSize = blockSize))
+for blockSize in smallBlockSizes:
+    with open("src/smallBlockTemplate.c") as templateFile:
+        template = Template(templateFile.read())
+        srcFileName = genSrcPath + "smallBlock{blockSize}.c".format(blockSize = blockSize)
+        with open(srcFileName, 'w') as outputFile:
+            outputFile.write(template.render(blockSize = blockSize))
+    with open("include/smallBlockTemplate.h") as templateFile:
+        template = Template(templateFile.read())
+        includeFileName = genIncludePath + "smallBlock{blockSize}.h".format(blockSize = blockSize)
+        with open(includeFileName, "w") as outputFile:
+            outputFile.write(template.render(blockSize = blockSize))
+    smallBlockHeader += "#include \"smallBlock{blockSize}.h\"\n".format(blockSize = blockSize)
+with open(smallBlockHeaderFileName, "w") as outputFile:
+    outputFile.write(smallBlockHeader)
