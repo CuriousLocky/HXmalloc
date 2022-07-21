11c2c0fe356f01b0ee5cdd435e8faa9c9ae47aaa
Hexiang Geng <hexiang@nclab.kaist.ac.kr>
2022-07-21 18:52:37 +0900

    Add header packing function

--- include/HXmalloc.h
+++ include/HXmalloc.h
@@ -31,6 +31,14 @@ static inline size_t align(size_t size, size_t alignment){
     return (((size)+(alignment-1)) & (~(alignment-1)));
 }
 
+static inline BlockHeader packHeader(size_t size, int index, uint64_t *superBlockBitmap){
+    uint64_t headerContent = 0;
+    headerContent |= (uint64_t)size >> 4 << (48 + 6);
+    headerContent |= (uint64_t)index << 48;
+    headerContent |= (uint64_t)superBlockBitmap;
+    return (BlockHeader)headerContent;
+}
+
 void *hxmalloc(size_t size);
 void hxfree(void *ptr);
 void *hxrealloc(void *ptr, size_t newSize);
